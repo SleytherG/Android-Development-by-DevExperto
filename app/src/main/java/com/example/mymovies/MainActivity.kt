2aaf5f28b0fe38945cc51f3aafc01a99f8489d1a
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2023-01-11 14:32:46 -0500

    Layout Row

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -44,18 +41,22 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun DefaultPreview() {
  MyMoviesTheme {
-  Column(
+  Row(
    modifier = Modifier.fillMaxSize(),
-   verticalArrangement = Arrangement.SpaceAround,
-   horizontalAlignment = Alignment.CenterHorizontally
+   horizontalArrangement = Arrangement.SpaceEvenly,
+//   verticalAlignment = Alignment.CenterVertically
   )
   {
-   Greeting("Sleyther", modifier = Modifier
-    .background(Color.LightGray)
-    .weight(2f));
-   Greeting("Juan", modifier = Modifier
-    .background(Color.Yellow)
-    .weight(1f));
+   Greeting(
+    "Sleyther", modifier = Modifier
+     .background(Color.LightGray)
+     .weight(2f)
+   )
+   Greeting(
+    "Juan", modifier = Modifier
+     .background(Color.Yellow)
+     .weight(1f)
+   )
 //   Greeting("Android", modifier = Modifier.background(Color.Cyan));
   }
  }
