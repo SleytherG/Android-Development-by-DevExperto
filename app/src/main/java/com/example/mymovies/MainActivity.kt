92625a6a2762d0df1332026408238438c33f93cd
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2023-01-11 14:54:33 -0500

    Modifiers

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mymovies.ui.theme.MyMoviesTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +28,7 @@ class MainActivity : ComponentActivity() {
      modifier = Modifier.fillMaxSize(),
      color = MaterialTheme.colors.background
     ) {
-     Greeting("Android")
+     ButtonText();
     }
    }
   }
@@ -37,27 +40,37 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
  Text(text = "Hello $name!", modifier = modifier)
 }
 
-@Preview(showSystemUi = true, name = "Android Greeting")
+//@Preview(showSystemUi = true)
 @Composable
-fun DefaultPreview() {
- MyMoviesTheme {
-  Row(
-   modifier = Modifier.fillMaxSize(),
-   horizontalArrangement = Arrangement.SpaceEvenly,
-//   verticalAlignment = Alignment.CenterVertically
-  )
-  {
-   Greeting(
-    "Sleyther", modifier = Modifier
-     .background(Color.LightGray)
-     .weight(2f)
-   )
-   Greeting(
-    "Juan", modifier = Modifier
-     .background(Color.Yellow)
-     .weight(1f)
-   )
-//   Greeting("Android", modifier = Modifier.background(Color.Cyan));
+fun MediaItem() {
+ Column() {
+  Box(
+   modifier = Modifier
+    .height(200.dp)
+    .fillMaxWidth()
+    .background(color = Color.Red)
+    .align(Alignment.CenterHorizontally)
+  ) {
+
   }
+  Text("Title 1")
+ }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ButtonText() {
+ Box(
+  modifier = Modifier.fillMaxSize(),
+  contentAlignment = Alignment.Center
+ ) {
+  Text(
+   text = "Hello World",
+   modifier = Modifier
+    .clickable { /*TODO*/ }
+    .background(Color.Cyan)
+    .border(width = 2.dp, color = Color.Blue)
+    .padding(horizontal = 16.dp, vertical = 8.dp)
+  )
  }
 }
