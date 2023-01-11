0e0f7dc92aba15131e8bc88b2fbe62bfed040de3
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2023-01-11 14:21:10 -0500

    first commit

---
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.mymovies
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mymovies.ui.theme.MyMoviesTheme
+
+class MainActivity : ComponentActivity() {
+ override fun onCreate(savedInstanceState: Bundle?) {
+  super.onCreate(savedInstanceState)
+  setContent {
+   MyMoviesTheme {
+    // A surface container using the 'background' color from the theme
+    Surface(
+     modifier = Modifier.fillMaxSize(),
+     color = MaterialTheme.colors.background
+    ) {
+     Greeting("Android")
+    }
+   }
+  }
+ }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+ Text(text = "Hello $name!", modifier = modifier)
+}
+
+@Preview(showSystemUi = true, name = "Android Greeting")
+@Composable
+fun DefaultPreview() {
+ MyMoviesTheme {
+  Box(
+   modifier = Modifier.fillMaxSize(),
+   contentAlignment = Alignment.Center
+  ) {
+   Greeting("Sleyther", modifier = Modifier.align(Alignment.BottomEnd))
+   Greeting("Juan")
+  }
+ }
+}
