b92d1b864328b3aa19b4acf4be3d6a49fbb12bce
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2023-01-11 18:01:39 -0500

    AppBar y Scaffold

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayCircleOutline
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -19,6 +19,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,90 +39,117 @@ class MainActivity : ComponentActivity() {
 //     ButtonText();
 //     MediaItem();
 //     MediaList();
-     val ( value, onValueChange ) = rememberSaveable { mutableStateOf("") };
-     StateSample(
-      value = value,
-      onValueChange = onValueChange);
+     /**
+      * Manejo del estado
+      */
+//     val ( value, onValueChange ) = rememberSaveable { mutableStateOf("") };
+//     StateSample(
+//      value = value,
+//      onValueChange = onValueChange);
+//    }
+
+     Scaffold(
+      topBar = {
+       TopAppBar(
+        title = { Text(text = stringResource(id = R.string.app_name)) },
+//        navigationIcon = {
+//         IconButton(onClick = { /*TODO*/ }) {
+//          Icon(imageVector = Icons.Default.Menu, contentDescription = null)
+//         }
+//        },
+        actions = {
+         IconButton(onClick = { /*TODO*/ }) {
+          Icon(imageVector = Icons.Default.Search, contentDescription = null)
+         }
+         IconButton(onClick = { /*TODO*/ }) {
+          Icon(imageVector = Icons.Default.Share, contentDescription = null)
+         }
+        }
+       )
+      }
+     ) { padding ->
+      MediaList(modifier = Modifier.padding(padding));
+     }
     }
    }
   }
  }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
- Text(
-  text = "Hello $name!",
-  modifier = modifier
- )
-}
-
-//@Preview(showSystemUi = true, device = Devices.PIXEL_4)
-@Composable
-fun StateSample(value: String, onValueChange: (String) -> Unit) {
- Column(
-  modifier = Modifier
-   .fillMaxSize()
-   .padding(64.dp),
-  verticalArrangement = Arrangement.Center,
-
-  ) {
-  TextField(
-   value = value,
-   onValueChange = { onValueChange(it) },
-   modifier = Modifier.fillMaxWidth()
-  )
+ @Composable
+ fun Greeting(name: String, modifier: Modifier = Modifier) {
   Text(
-   text = value,
-   modifier = Modifier
-    .fillMaxWidth()
-    .background(Color.Yellow)
-    .padding(8.dp)
+   text = "Hello $name!",
+   modifier = modifier
   )
-  Button(
-   onClick = { onValueChange("") },
-   modifier = Modifier.fillMaxWidth(),
-   enabled = value.isNotEmpty()
-  ) {
-   Text(text = "Clear")
+ }
+
+ //@Preview(showSystemUi = true, device = Devices.PIXEL_4)
+ @Composable
+ fun StateSample(value: String, onValueChange: (String) -> Unit) {
+  Column(
+   modifier = Modifier
+    .fillMaxSize()
+    .padding(64.dp),
+   verticalArrangement = Arrangement.Center,
+
+   ) {
+   TextField(
+    value = value,
+    onValueChange = { onValueChange(it) },
+    modifier = Modifier.fillMaxWidth()
+   )
+   Text(
+    text = value,
+    modifier = Modifier
+     .fillMaxWidth()
+     .background(Color.Yellow)
+     .padding(8.dp)
+   )
+   Button(
+    onClick = { onValueChange("") },
+    modifier = Modifier.fillMaxWidth(),
+    enabled = value.isNotEmpty()
+   ) {
+    Text(text = "Clear")
+   }
   }
  }
-}
 
-@OptIn(ExperimentalFoundationApi::class)
+ @OptIn(ExperimentalFoundationApi::class)
 //@Preview(showSystemUi = true)
-@Composable
-fun MediaList() {
- LazyVerticalGrid(
-  contentPadding = PaddingValues(2.dp),
-  cells = GridCells.Adaptive(200.dp),
-  content = {
-   items(getMedia()) { item ->
-    MediaListItem(item, modifier = Modifier.padding(2.dp));
-   }
-  },
+ @Composable
+ fun MediaList(modifier: Modifier = Modifier) {
+  LazyVerticalGrid(
+   contentPadding = PaddingValues(2.dp),
+   cells = GridCells.Adaptive(200.dp),
+   modifier = modifier,
+   content = {
+    items(getMedia()) { item ->
+     MediaListItem(item, modifier = Modifier.padding(2.dp));
+    }
+   },
 
-  )
-}
-
-//@Preview(showSystemUi = true)
-@Composable
-fun MediaListItem(item: MediaItem, modifier: Modifier = Modifier) {
- Column(
-  modifier = modifier
- ) {
-  Box(
-   modifier = Modifier
-    .height(200.dp)
-    .fillMaxWidth(),
-//   contentAlignment = Alignment.Center
-  ) {
-   AsyncImage(
-    model = item.thumb,
-    contentDescription = null,
-    modifier = Modifier.fillMaxSize(),
-    contentScale = ContentScale.Crop
    )
+ }
+
+ //@Preview(showSystemUi = true)
+ @Composable
+ fun MediaListItem(item: MediaItem, modifier: Modifier = Modifier) {
+  Column(
+   modifier = modifier
+  ) {
+   Box(
+    modifier = Modifier
+     .height(200.dp)
+     .fillMaxWidth(),
+//   contentAlignment = Alignment.Center
+   ) {
+    AsyncImage(
+     model = item.thumb,
+     contentDescription = null,
+     modifier = Modifier.fillMaxSize(),
+     contentScale = ContentScale.Crop
+    )
 //   Image(
 //    painter = rememberImagePainter(data = "https://picsum.photos/400/400", builder = {
 //     crossfade(true)
@@ -130,52 +158,53 @@ fun MediaListItem(item: MediaItem, modifier: Modifier = Modifier) {
 //    modifier = Modifier.fillMaxSize(),
 //    contentScale = ContentScale.Crop
 //   );
-   if (item.type == MediaItem.Type.VIDEO) {
-    Icon(
-     imageVector = Icons.Default.PlayCircleOutline,
-     contentDescription = null,
-     modifier = Modifier
-      .size(92.dp)
-      .align(Alignment.Center),
-     tint = Color.White,
+    if (item.type == MediaItem.Type.VIDEO) {
+     Icon(
+      imageVector = Icons.Default.PlayCircleOutline,
+      contentDescription = null,
+      modifier = Modifier
+       .size(92.dp)
+       .align(Alignment.Center),
+      tint = Color.White,
+     )
+    }
+   }
+   Box(
+    modifier = Modifier
+     .fillMaxWidth()
+     .background(MaterialTheme.colors.secondary)
+     .padding(16.dp),
+    contentAlignment = Alignment.Center
+   ) {
+    Text(
+     text = item.title,
+     style = MaterialTheme.typography.h6
     )
+
    }
   }
+ }
+
+ //@Preview(showSystemUi = true)
+ @Composable
+ fun ButtonText() {
   Box(
-   modifier = Modifier
-    .fillMaxWidth()
-    .background(MaterialTheme.colors.secondary)
-    .padding(16.dp),
+   modifier = Modifier.fillMaxSize(),
    contentAlignment = Alignment.Center
   ) {
    Text(
-    text = item.title,
-    style = MaterialTheme.typography.h6
-   )
-
-  }
- }
-}
-
-//@Preview(showSystemUi = true)
-@Composable
-fun ButtonText() {
- Box(
-  modifier = Modifier.fillMaxSize(),
-  contentAlignment = Alignment.Center
- ) {
-  Text(
-   text = "Hello World",
-   maxLines = 1,
-   softWrap = false,
-   overflow = TextOverflow.Ellipsis,
-   style = MaterialTheme.typography.h4.copy(
-    shadow = Shadow(
-     offset = Offset(5f, 5f),
-     blurRadius = 5f,
-     color = Color.Black.copy(alpha = 0.5f)
+    text = "Hello World",
+    maxLines = 1,
+    softWrap = false,
+    overflow = TextOverflow.Ellipsis,
+    style = MaterialTheme.typography.h4.copy(
+     shadow = Shadow(
+      offset = Offset(5f, 5f),
+      blurRadius = 5f,
+      color = Color.Black.copy(alpha = 0.5f)
+     )
     )
    )
-  )
+  }
  }
 }
