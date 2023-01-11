18fa903e0a6d5d315a3892fd2a53d69aa059222e
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2023-01-11 15:33:12 -0500

    Cargar imagenes e iconos en Jetpack Compose

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -3,19 +3,29 @@ package com.example.mymovies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.PlayCircleOutline
+import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -28,6 +38,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import coil.transform.BlurTransformation
+import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
 import com.example.mymovies.ui.theme.MyMoviesTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +55,8 @@ class MainActivity : ComponentActivity() {
      modifier = Modifier.fillMaxSize(),
      color = MaterialTheme.colors.background
     ) {
-     ButtonText();
+//     ButtonText();
+     MediaItem();
     }
    }
   }
@@ -51,7 +67,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
  Text(
   text = "Hello $name!",
-  modifier = modifier)
+  modifier = modifier
+ )
 }
 
 @Preview(showSystemUi = true)
@@ -61,10 +78,23 @@ fun MediaItem() {
   Box(
    modifier = Modifier
     .height(200.dp)
-    .fillMaxWidth()
-    .background(color = Color.Red)
+    .fillMaxWidth(),
+   contentAlignment = Alignment.Center
   ) {
-
+   Image(
+    painter = rememberImagePainter(data = "https://picsum.photos/400/400", builder = {
+     crossfade(true)
+    }),
+    contentDescription = null,
+    modifier = Modifier.fillMaxSize(),
+    contentScale = ContentScale.Crop
+   );
+   Icon(
+    imageVector = Icons.Default.PlayCircleOutline,
+    contentDescription = null,
+    modifier = Modifier.size(92.dp),
+    tint = Color.White,
+   )
   }
   Box(
    modifier = Modifier
